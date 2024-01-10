fcd1d3f0a02ea73b040a23c9004e5277f3266a7b
Vachek-hooves <vachek0602@gmail.com>
2024-01-10 13:35:51 +0200

    another option for extraRefucer

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -50,6 +50,16 @@ const booksSlice = createSlice({
       // );
     },
   },
+  // OPTION 1
+  // extraReducers: {
+  // computed property
+  //   [fetchBook.fulfilled]: (state, action) => {
+  //     if (action.payload.title && action.payload.author) {
+  //       state.push(createBookWithId(action.payload, 'API'));
+  //     }
+  //   },
+
+  // OPTION 2
   // to integrate async thunk function need to use extraReducers
   extraReducers: (builder) => {
     builder.addCase(fetchBook.fulfilled, (state, action) => {
